8ab5a7e91ca90ab73ccea495077fd82da3bbae6b
a2kaido <amane.nikaido+a2kaido@gmail.com>
2020-12-20 22:54:06 +0900

    hello compose

--- app/src/main/java/io/a2kaido/mikan/MainActivity.kt
+++ app/src/main/java/io/a2kaido/mikan/MainActivity.kt
@@ -2,10 +2,30 @@ package io.a2kaido.mikan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MaterialTheme {
+                Greeting("mikan")
+            }
+        }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text (text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Compose")
 }
