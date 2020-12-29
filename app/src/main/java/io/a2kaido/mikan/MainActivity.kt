b9b5e9a26116588c3963a20cb061267cfa05bd06
a2kaido <amane.nikaido+a2kaido@gmail.com>
2020-12-29 20:25:08 +0900

    add click event

--- app/src/main/java/io/a2kaido/mikan/MainActivity.kt
+++ app/src/main/java/io/a2kaido/mikan/MainActivity.kt
@@ -2,7 +2,9 @@ package io.a2kaido.mikan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,6 +13,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,13 +43,19 @@ fun ClippingList() {
 
 @Composable
 fun ClippingItem(title: String, description: String) {
+    val context = AmbientContext.current
     Card(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth(),
         elevation = 4.dp
     ) {
-        Column {
+        Column(
+            Modifier
+                .clickable(onClick = {
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show()
+                })
+        ) {
             Text(
                 modifier = Modifier.padding(start = 8.dp, top = 8.dp),
                 text = title
